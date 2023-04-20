a2127f59f11e77c3d6e109a8d73ac73e49a89516
jominj533 <84025851+jominj533@users.noreply.github.com>
2023-04-19 23:02:24 -0400

    Update RentalModel.php

--- PHP final/models/RentalModel.php
+++ PHP final/models/RentalModel.php
@@ -29,12 +29,24 @@
         $table = self::$_table;
             $sql = "INSERT INTO {$table} (
                 owner_name
+                address
+                contact_email
+                contact_phone_number
+                city_id
             ) VALUES (
                 :owner_name
+                :address
+                :contact_email
+                :contact_phone_number
+                :city_id
             )";
             $conn = get_connection();
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(":owner_name",$package["owner_name"], PDO::PARAM_STR);
+            $stmt->bindParam(":address",$package["address"], PDO::PARAM_STR);
+            $stmt->bindParam(":contact_email",$package["contact_email"], PDO::PARAM_STR);
+            $stmt->bindParam(":contact_phone_number",$package["contact_phone_number"], PDO::PARAM_INT);
+            $stmt->bindParam(":city_id",$package["city_id"], PDO::PARAM_INT);
             $stmt->execute();
             $conn = null;
         }
@@ -42,12 +54,20 @@
             $table = self::$_table;
             $sql = "UPDATE {$table} SET 
                 owner_name = :owner_name
+                address = :address
+                contact_email= :contact_email
+                contact_phone_number = :contact_phone_number
+                city_id =:city_id
             WHERE id = id";
 
             $conn = get_connection();
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(":id",$package["id"], PDO::PARAM_INT);
             $stmt->bindParam(":owner_name",$package["owner_name"], PDO::PARAM_STR);
+            $stmt->bindParam(":address",$package["address"], PDO::PARAM_STR);
+            $stmt->bindParam(":contact_email",$package["contact_email"], PDO::PARAM_STR);
+            $stmt->bindParam(":contact_phone_number",$package["contact_phone_number"], PDO::PARAM_INT);
+            $stmt->bindParam(":city_id",$package["city_id"], PDO::PARAM_INT);
             $stmt->execute();
             $conn = null;
         }
@@ -60,7 +80,6 @@
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(":id",$id, PDO::PARAM_INT);
             $stmt->execute();
-
             $conn = null;
         }
 
